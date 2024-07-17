cb4099c01f19eeec729c392da91315059abf1621
T4ti4n4 <rooney2001.tbg@gmail.com>
2024-07-17 15:21:12 -0500

    Configuración HTML ddel index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div>
+            <label for="user">User</label>
+            <input type="email" name="user">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password">
+        </div>
+    <button>Iniciar sesión</button>
+    </form>
+</body>
+</html>
